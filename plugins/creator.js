b23c4c8918738682beb370e629ac4a89baac8e6f
Aditchan-XD <80593224+Aditchan-XD@users.noreply.github.com>
2021-04-29 06:02:48 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  // this.sendContact(m.chat, '6281313293689', 'Aditya', m)
   this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
 }
 handler.help = ['owner', 'creator']
